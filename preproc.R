4dc90cc5c0871845e1433dfb26d3e668b3cf7ea0
Magdalena Gontarz <magda1428@gmail.com>
2015-07-26 17:56:53 +0200

    changes in preprocessing - gender added

--- preproc.R
+++ preproc.R
@@ -1,12 +1,15 @@
 
 cit1 <- read.csv("apddb_x1.csv", sep=";", quote = "", 
-                row.names = NULL, 
-                stringsAsFactors = FALSE)
+                 row.names = NULL, 
+                 stringsAsFactors = FALSE)
 cit2 <- read.csv("apddb_x2.csv", sep=";", quote = "", 
-                row.names = NULL, header=TRUE,
-                stringsAsFactors = FALSE)
+                 row.names = NULL, header=TRUE,
+                 stringsAsFactors = FALSE)
+cit3 <- read.csv("apddb_x3.csv", sep=";", quote = "", 
+                 row.names = NULL, header=TRUE,
+                 stringsAsFactors = FALSE)
 
-cit<-rbind(cit1,cit2)
+cit<-rbind(cit1,cit2,cit3)
 
 cit[cit=='brak']=NA
 cit[cit=='(brak)']=NA
@@ -21,11 +24,11 @@ cit$lang[cit$lang=="angielski [EN]"]="en"
 #bachelor fields
 idxs = grepl("Licencjat z fizyki",cit$field)
 cit$field[idxs]="fiz"
-idxs = grepl("Licencjat z zastosowaÅ„ fizyki",cit$field)
+idxs = grepl("Licencjat z zastosowañ fizyki",cit$field)
 cit$field[idxs]="zfbm"
 idxs = grepl("Licencjat z astronomii",cit$field)
 cit$field[idxs]="ast"
-idxs = grepl("Licencjat z inÅ¼ynierii nano",cit$field)
+idxs = grepl("Licencjat z in¿ynierii nano",cit$field)
 cit$field[idxs]="in"
 #master fields
 idxs = grepl("Magisterium z fizyki",cit$field)
@@ -34,18 +37,29 @@ idxs = grepl("Magisterium na kierunku fizyka",cit$field)
 cit$field[idxs]="fiz"
 idxs = grepl("Magisterium na kier. fizyka",cit$field)
 cit$field[idxs]="fiz"
-idxs = grepl("Magisterium z zastosowaÅ„ fizyki",cit$field)
+idxs = grepl("Magisterium z zastosowañ fizyki",cit$field)
 cit$field[idxs]="zfbm"
 idxs = grepl("Magisterium z astronomii",cit$field)
 cit$field[idxs]="ast"
-idxs = grepl("Magisterium z inÅ¼ynierii nano",cit$field)
+idxs = grepl("Magisterium z in¿ynierii nano",cit$field)
 cit$field[idxs]="in"
 
 cit<-cit[-which((nchar(cit$field)>4)),]
-cit<-cit[-which((nchar(cit$field)<2)),]
+#cit<-cit[-which((nchar(cit$field)<2)),]
 cit<-cit[-which(is.na(cit$field)),]
 
 # name to gender
+zx<-strsplit(cit$author," ")
+
+zx[sapply(zx,length)<3]=TRUE
+zx[sapply(zx,length)>2]=FALSE
+cit$author(which(unlist(zx)))
+cit$author<-gsub("mgr ", "", cit$author)
+cit$author<-gsub("in¿. ", "", cit$author)
+
+df <- data.frame(matrix(unlist(strsplit(cit$author," ")), nrow=dim(cit)[1], byrow=T),stringsAsFactors=FALSE)
+cit$author[grep("a$",df$X1)]="F"
+cit$author[-grep("a$",df$X1)]="M"
 
 # date string to date format
 cit$data<-gsub("stycznia", "sty", cit$data)
@@ -55,9 +69,14 @@ cit$data<-gsub("kwietnia", "kwi", cit$data)
 cit$data<-gsub("maja", "maj", cit$data)
 cit$data<-gsub("czerwca", "cze", cit$data)
 cit$data<-gsub("lipca", "lip", cit$data)
-cit$data<-gsub("wrzeÅ›nia", "wrz", cit$data)
-cit$data<-gsub("paÅºdziernika", "paÅº", cit$data)
+cit$data<-gsub("wrzeœnia", "wrz", cit$data)
+cit$data<-gsub("paŸdziernika", "paŸ", cit$data)
 cit$data<-gsub("listopada", "lis", cit$data)
 cit$data<-gsub("grudnia", "gru", cit$data)
-
 cit$data<-as.Date(cit$data,"%d %b %Y")
+
+nazwy_kol=colnames(cit)
+nazwy_kol[6]="gender"
+colnames(cit)=nazwy_kol
+save(cit,file="apd.Rda")
+
